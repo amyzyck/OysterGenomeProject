74a138349b3f6c13627f10e48b19f874c614b5f5
K. Bodie Weedop <kbweedop@gmail.com>
2019-10-19 09:13:53 -0400

    Update and rename outliers-by-region.sh to intersect-outliers-regions.sh

--- popstructureOutliers/src/9outliers-by-region/intersect-outliers-regions.sh
+++ popstructureOutliers/src/9outliers-by-region/intersect-outliers-regions.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Download data from Jon's KITT server
 wget http://kitt.uri.edu/sorted.ref3.0*
+mkdir GenomicRegionFiles
+mv -r sorted.ref3.0* GenomicRegionFiles/
 # Finding which outliers intersect with each region identified
 bedtools intersect -u -a FstOutliers_Unrelated.bed -b GenomicRegionFiles/sorted.ref3.0.CDS.bed > Outliers_CDS.txt
-bedtools intersect -u -a FstOutliers_Unrelated.bed -b GenomicRegionFiles/sorted.ref3.0.CDS.sc.bed > Outliers_CDS_sc.txt
 bedtools intersect -u -a FstOutliers_Unrelated.bed -b GenomicRegionFiles/sorted.ref3.0.exon.bed > Outliers_exon.txt
-bedtools intersect -u -a FstOutliers_Unrelated.bed -b GenomicRegionFiles/sorted.ref3.0.exon.sc.bed > Outliers_exon_sc.txt
 bedtools intersect -u -a FstOutliers_Unrelated.bed -b GenomicRegionFiles/sorted.ref3.0.UTR.bed > Outliers_UTR.txt
 bedtools intersect -u -a FstOutliers_Unrelated.bed -b GenomicRegionFiles/sorted.ref3.intergenic.bed > Outliers_Intergenic.txt
 bedtools intersect -u -a FstOutliers_Unrelated.bed -b GenomicRegionFiles/sorted.ref3.0.gene.bed > Outliers_gene.txt
-bedtools intersect -u -a FstOutliers_Unrelated.bed -b GenomicRegionFiles/sorted.ref3.0.gene.sc.bed > Outliers_gene_sc.txt
